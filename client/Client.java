3ebc9cd9e908404327a55888ce4a8c7d8eceb6a5
Joshua Michael Waggoner <rabbitfighter81@gmail.com>
2014-11-24 13:02:52 -0700

    Update Client.java

--- client/Client.java
+++ client/Client.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.Date;
 import java.util.Scanner;
+
 import javax.net.ssl.*;
 
 /**
@@ -34,9 +35,9 @@ public class Client {
 			// New SSL Socket
 			SSLSocket socket = (SSLSocket) sslSocketFactory.createSocket(
 					hostName, portNumber);
+			// 
 			System.out.println("Client connected at port <" + portNumber
 					+ "> on <" + hostName + ">");
-
 			// Print writer for output stream
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			// Buffered reader for input
@@ -89,24 +90,27 @@ public class Client {
 	}
 
 	/**
-	 * Prints the
+	 * Prints the session info
 	 * 
 	 * @param sslSession
 	 */
 	public static void printSessionInfo(SSLSession sslSession) {
-		System.out
-				.println("\n**********************************************************************");
-		System.out.println("New connection established at peer port <"
-				+ sslSession.getPeerPort() + ">");
-		System.out.println("Peer host is: " + sslSession.getPeerHost());
-		System.out.println("Cipher suite is: " + sslSession.getCipherSuite());
-		System.out.println("Protocol is: " + sslSession.getProtocol());
-		System.out.println("Session ID is: " + sslSession.getId());
-		System.out.println("The creation time of this session is: "
-				+ new Date(sslSession.getCreationTime()));
-		System.out.println("Last accessed time of this session is: "
-				+ new Date(sslSession.getLastAccessedTime()));
-
-
+		
+		if (sslSession.isValid()) {
+			
+			System.out.println("\nNew connection established at peer port <"
+					+ sslSession.getPeerPort() + ">");
+			System.out.println("Peer host is: " + sslSession.getPeerHost());
+			System.out.println("Cipher suite is: " + sslSession.getCipherSuite());
+			System.out.println("Protocol is: " + sslSession.getProtocol());
+			System.out.println("Session ID is: " + sslSession.getId());
+			System.out.println("The creation time of this session is: "
+					+ new Date(sslSession.getCreationTime()));
+			System.out.println("Last accessed time of this session is: "
+					+ new Date(sslSession.getLastAccessedTime()));
+		} else {
+			System.out.println("\nSession is invalid");
+		}
+		
 	}
 }
